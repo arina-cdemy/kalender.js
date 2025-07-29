1333719255b02f0b37230ff08069dafc20e4ddb9
arina-cdemy <arina.misakyan.cdemy@gmail.com>
2025-07-29 18:31:57 +0200

    add date formatting functionality to display important events

---
+++ script.js
@@ -0,0 +1,11 @@
+const d = new Date(2025, 6, 14, 0, 0); // July is month 6 (zero-based index)
+const date = d.getDate();
+const month = d.getMonth() + 1; // Months are zero-based in JavaScript
+const year = d.getFullYear();
+const formattedDate = `${date}.${month}.${year}`;
+
+
+console.log(formattedDate); // Outputs: 14.7.2025
+
+
+document.getElementById("überschrift").innerHTML = "Wichtige Ereignisse am " + formattedDate;
